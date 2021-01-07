a5cf7ccd89608176b46ce72eb4724e621e8fb2ea
jackagoodguy <jak-3@163.com>
2021-01-07 09:23:36 +0800

    modify

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+#ps aux | grep java | grep ${NAME} | xargs kill -9
 java -jar target/${NAME}-${VERSION}.jar
